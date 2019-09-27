9780ba370b297abf74ed1a2467fa94fee977b0f9
Nkechi B. Nnaji <nkechi.nnaji@udc.edu>
2019-09-27 08:31:53 -0500

    Fixed selection level screen

---
+++ app/src/main/java/sarveshchavan777/quizgame/LevelSelectionActivity.java
@@ -0,0 +1,51 @@
+package sarveshchavan777.quizgame;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by LENOVO on 12/12/2016.
+ * Modified by Nkechi Nnaji, Sept 26 2019
+ */
+
+public class LevelSelectionActivity extends AppCompatActivity {
+
+    CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_selection);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.selection_level_toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+//            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+            toolbar.setTitle("Home");
+        }
+        cardView = (CardView) findViewById(R.id.btn_easy);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LevelSelectionActivity.this, QuestionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.selection_level_menu, menu);
+        return true;
+    }
+}
