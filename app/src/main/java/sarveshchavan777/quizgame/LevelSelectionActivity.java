3aafda7932bab19fff2756e820072e742bb4746e
nkechinnaji <nkechi_nnaji@gallup.com>
2019-09-30 22:50:01 -0500

    Added comments

--- app/src/main/java/sarveshchavan777/quizgame/LevelSelectionActivity.java
+++ app/src/main/java/sarveshchavan777/quizgame/LevelSelectionActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 /**
  * Created by LENOVO on 12/12/2016.
  * Modified by Nkechi Nnaji, Sept 26 2019
+ * Description: Homescreen for selecting levels
  */
 
 public class LevelSelectionActivity extends AppCompatActivity {
