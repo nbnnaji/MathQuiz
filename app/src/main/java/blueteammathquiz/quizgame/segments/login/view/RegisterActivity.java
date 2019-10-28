9f39e66890c102895dacb46a1df6acb9270cbf6b
nkechinnaji <nkechi_nnaji@gallup.com>
2019-10-27 22:45:05 -0500

    Code cleanup

--- app/src/main/java/blueteammathquiz/quizgame/segments/login/view/RegisterActivity.java
+++ app/src/main/java/blueteammathquiz/quizgame/segments/login/view/RegisterActivity.java
@@ -24,10 +24,10 @@ import java.util.Map;
 
 import blueteammathquiz.quizgame.R;
 
+//Added By Nkechi Nnaji
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "RegisterActivity";
-    private static final String URL_FOR_REGISTRATION = "https://XXX.XXX.X.XX/android_login_example/register.php";
     ProgressDialog progressDialog;
 
     private EditText signupInputName, signupInputEmail, signupInputPassword, signupInputAge;
@@ -40,128 +40,5 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
-//
-//        // Progress dialog
-//        progressDialog = new ProgressDialog(this);
-//        progressDialog.setCancelable(false);
-//
-//        signupInputName = (EditText) findViewById(R.id.signup_input_name);
-//        signupInputEmail = (EditText) findViewById(R.id.signup_input_email);
-//        signupInputPassword = (EditText) findViewById(R.id.signup_input_password);
-//        signupInputAge = (EditText) findViewById(R.id.signup_input_age);
-//
-//        btnSignUp = (Button) findViewById(R.id.btn_signup);
-//        btnLinkLogin = (Button) findViewById(R.id.btn_link_login);
-//
-//        genderRadioGroup = (RadioGroup) findViewById(R.id.gender_radio_group);
-//        btnSignUp.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                submitForm();
-//            }
-//        });
-//        btnLinkLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                Intent i = new Intent(getApplicationContext(),LoginActivity.class);
-//                startActivity(i);
-//            }
-//        });
-//    }
-//
-//    private void submitForm() {
-//
-//        int selectedId = genderRadioGroup.getCheckedRadioButtonId();
-//        String gender;
-//        if(selectedId == R.id.female_radio_btn)
-//            gender = "Female";
-//        else
-//            gender = "Male";
-//
-//        registerUser(signupInputName.getText().toString(),
-//                signupInputEmail.getText().toString(),
-//                signupInputPassword.getText().toString(),
-//                gender,
-//                signupInputAge.getText().toString());
-//    }
-//
-//    private void registerUser(final String name,  final String email, final String password,
-//                              final String gender, final String dob) {
-//        // Tag used to cancel the request
-//        String cancel_req_tag = "register";
-//
-//        progressDialog.setMessage("Adding you ...");
-//        showDialog();
-//
-//        StringRequest strReq = new StringRequest(Request.Method.POST,
-//                URL_FOR_REGISTRATION, new Response.Listener<String>() {
-//
-//            @Override
-//            public void onResponse(String response) {
-//                Log.d(TAG, "Register Response: " + response.toString());
-//                hideDialog();
-//
-//                try {
-//                    JSONObject jObj = new JSONObject(response);
-//                    boolean error = jObj.getBoolean("error");
-//
-//                    if (!error) {
-//                        String user = jObj.getJSONObject("user").getString("name");
-//                        Toast.makeText(getApplicationContext(), "Hi " + user +", You are successfully Added!", Toast.LENGTH_SHORT).show();
-//
-//                        // Launch login activity
-//                        Intent intent = new Intent(
-//                                RegisterActivity.this,
-//                                LoginActivity.class);
-//                        startActivity(intent);
-//                        finish();
-//                    } else {
-//
-//                        String errorMsg = jObj.getString("error_msg");
-//                        Toast.makeText(getApplicationContext(),
-//                                errorMsg, Toast.LENGTH_LONG).show();
-//                    }
-//                }
-//
-//                catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//
-//            }
-//        }, new Response.ErrorListener() {
-//
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.e(TAG, "Registration Error: " + error.getMessage());
-//                Toast.makeText(getApplicationContext(),
-//                        error.getMessage(), Toast.LENGTH_LONG).show();
-//                hideDialog();
-//            }
-//        }) {
-//            @Override
-//            protected Map<String, String> getParams() {
-//                // Posting params to register url
-//                Map<String, String> params = new HashMap<String, String>();
-//                params.put("name", name);
-//                params.put("email", email);
-//                params.put("password", password);
-//                params.put("gender", gender);
-//                params.put("age", dob);
-//                return params;
-//            }
-//        };
-//        // Adding request to request queue
-//        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq, cancel_req_tag);
-//    }
-//
-//    private void showDialog() {
-//        if (!progressDialog.isShowing())
-//            progressDialog.show();
-//    }
-//
-//    private void hideDialog() {
-//        if (progressDialog.isShowing())
-//            progressDialog.dismiss();
-//    }
+
 }
